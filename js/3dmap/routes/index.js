59365711f90c26c8871a9268d8479f1be5962f3b
sylvesterchiang <sylvester_ch7593@hotmail.com>
2015-11-07 11:50:47 -0500

    running

--- js/3dmap/routes/index.js
+++ js/3dmap/routes/index.js
@@ -11,28 +11,26 @@ router.get('/', function(req, res, next) {
   var options = {
         host: 'maps.googleapis.com',
         //path: '/maps/api/directions/json?origin=Toledo&destination=Madrid&region=es&key=AIzaSyC6U0ex_0ClX8ZiEU9schuHfIFSyQ1KXKE'
-  		path: '/maps/api/geocode/xml?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyC6U0ex_0ClX8ZiEU9schuHfIFSyQ1KXKE'
-  };
+  		path: '/maps/api/directions/json?origin=Toledo&destination=Madrid&region=es&key=AIzaSyC6U0ex_0ClX8ZiEU9schuHfIFSyQ1KXKE'
+  }
 
-  https.get('https://maps.googleapis.com/maps/api/directions/json?origin=Toledo&destination=Madrid&region=es&key=AIzaSyC6U0ex_0ClX8ZiEU9schuHfIFSyQ1KXKE', function(resp){
-  	//resp.setEncoding('utf8');
-  	//res.charset = 'value'
-  	//res.writeHead(200, {"Content-Type": "application/json"})
-  	//resp.set({ 'content-type': 'application/json; charset=utf-8' })
-  	body = "";
+	callback = function(response) {
+	   // variable that will save the result
+	   var result = '';
 
-  	resp.on('data', function(d) {
-  		//var jsonObject = JSON.parse(JSON.stringify(d));
+	   // every time you have a new piece of the result
+	   response.on('data', function(chunk) {
+	      result += chunk;
+	   });
 
-  		//var jsonObject = JSON.parse(JSON.stringify(d.replace('"', '')));
-  		//var jsonObject = eval("(" + d + ")")
-  		body += d
-	  	
-	  	console.log(JSON.parse(body))
-		//console.log(Object.getOwnPropertyNames ( d ))
-	});
-  });
+	   // when you get everything back
+	   response.on('end', function() {
+	     res.send(result);
+	     console.log(JSON.parse(result))
+	   });
+	}
 
+	https.request(options, callback).end();
 });
 
 module.exports = router;
