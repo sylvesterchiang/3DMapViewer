91782c66b99b06c23ccb0d85825c80134793318f
sylvesterchiang <sylvester_ch7593@hotmail.com>
2015-11-07 14:52:37 -0500

    got json

--- js/3dmap/routes/index.js
+++ js/3dmap/routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var https = require('https');
+var jquery = require('jQuery');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,11 +9,15 @@ router.get('/', function(req, res, next) {
   console.log(req.query.test)
   console.log(req.query.what)
 
+  directions = []
+
   var options = {
         host: 'maps.googleapis.com',
         //path: '/maps/api/directions/json?origin=Toledo&destination=Madrid&region=es&key=AIzaSyC6U0ex_0ClX8ZiEU9schuHfIFSyQ1KXKE'
   		path: '/maps/api/directions/json?origin=Toledo&destination=Madrid&region=es&key=AIzaSyC6U0ex_0ClX8ZiEU9schuHfIFSyQ1KXKE'
   }
+
+  var consoleCallback = console.log;
 
 	callback = function(response) {
 	   // variable that will save the result
@@ -26,9 +31,18 @@ router.get('/', function(req, res, next) {
 	   // when you get everything back
 	   response.on('end', function() {
 	     res.send(result);
-	     console.log(JSON.parse(result))
+	     console.log(JSON.parse(result).routes)
+	     legs = JSON.parse(result).routes[0].legs
+
+	     for (var i = 0; i < legs.length; i++ ){
+	     	directions.push(legs[i])
+	     }
+
+	     console.log(directions)
 	   });
 	}
+
+
 
 	https.request(options, callback).end();
 });
